5251f9290090ac1e497b86821fccf41276ddb27e
shankkalra <shankkalra@gmail.com>
2021-10-16 19:47:27 +0530

    System Property

--- src/main/java/com/utils/owner/FrameworkConfig.java
+++ src/main/java/com/utils/owner/FrameworkConfig.java
@@ -3,7 +3,7 @@ package com.utils.owner;
 import org.aeonbits.owner.Config;
 import java.util.List;
 
-@Config.Sources(value="file:/home/shanky/Personal/Azure/Practice/SDET_QA/src/main/resources/FrameworkConfig.properties")
+@Config.Sources(value="file:${user.dir}/src/main/resources/FrameworkConfig.properties")
 public interface FrameworkConfig extends Config {
 
     String browser();
