c23503779f44bdbd8052d8b1b9dea191f7ebe049
shankkalra <shankkalra@gmail.com>
2021-10-17 23:40:26 +0530

    Test Data

---
+++ src/test/java/com/utils/zerocell/TestData.java
@@ -0,0 +1,13 @@
+package com.utils.zerocell;
+
+public class TestData {
+
+    //POJO
+
+    private String TestName;
+    private String FirstName;
+    private String Email;
+    private String Gender;
+
+
+}
